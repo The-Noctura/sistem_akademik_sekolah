0876236356140347f4b3641910de0b9aed886d17
The-Noctura <muhamadalghani584@gmail.com>
2026-08-15 20:12:44 +0700

    chore: setup modul jadwal H2
    - Buat folder views admin/jadwal, guru/jadwal, dan siswa/jadwal
    - Inisialisasi index.blade.php untuk admin jadwal

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
+});
+
+Route::get('/admin/jadwal/', function () {
+  return view('admin.jadwal.index');
 });
