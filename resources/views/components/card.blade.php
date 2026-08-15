5338ae42733bbfd26d4dad2b5d476873e0131c1a
Nabil Yusra <nabilazura3@gmail.com>
2026-08-15 21:54:23 +0700

    feat: add Card component for content wrapping with styling

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,14 @@
+{{-- 
+FILE: resources/views/components/card.blade.php
+KOMPONEN: Card
+FUNGSI: Membungkus konten dalam card dengan border dan shadow
+--}}
+
+@props(['title' => null])
+
+<div {{ $attributes->merge(['class' => 'bg-surface border border-slate-200 rounded-lg p-6 shadow-sm']) }}>
+    @if($title)
+        <h3 class="text-lg font-semibold mb-4">{{ $title }}</h3>
+    @endif
+    {{ $slot }}
+</div>
