773e8ed0d140cc0cc87b274d910ba35a9730ea70
Muhammad Rizqi Pratama <awsgar55@gmail.com>
2026-08-29 09:53:47 +0700

    feat(model): add Kelas model

---
+++ app/Models/Kelas.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Kelas extends Model
+{
+  public function siswa()
+{
+    return $this->hasMany(Siswa::class);
+}
+
+public function waliKelas()
+{
+    return $this->belongsTo(Guru::class, 'wali_kelas_id');
+}
+}
