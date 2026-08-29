332ad861fa596b065dc0db06df8e0d1492d99007
Muhammad Rizqi Pratama <awsgar55@gmail.com>
2026-08-29 10:07:13 +0700

    feat(model): add Mengajar model

---
+++ app/Models/Mengajar.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mengajar extends Model
+{
+public function guru()
+{
+    return $this->belongsTo(Guru::class);
+}
+
+public function mapel()
+{
+    return $this->belongsTo(Mapel::class);
+}
+
+public function kelas()
+{
+    return $this->belongsTo(Kelas::class);
+}
+}
