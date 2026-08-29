c22d036b6d62625c0d140b661af7ff1a819ba56a
Muhammad Rizqi Pratama <awsgar55@gmail.com>
2026-08-29 10:08:38 +0700

    refactor(model): update Nilai model

--- app/Models/Nilai.php
+++ app/Models/Nilai.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Jadwal;
 
 class Nilai extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'nilai';
+
+    protected $fillable = [
+        'siswa_id',          // foreign key ke users.id
+        'mata_pelajaran_id', // foreign key ke jadwal.id
+        'nilai',
+        'predikat',
+    ];
+
+    protected $casts = [
+        'nilai' => 'integer',
+    ];
+
+    public function siswa()
+    {
+        return $this->belongsTo(User::class, 'siswa_id');
+    }
+
+    public function mataPelajaran()
+    {
+        return $this->belongsTo(Jadwal::class, 'mata_pelajaran_id');
+    }
 }
